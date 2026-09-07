abd066ad59f2a5df8f702e560d66191ac225334c
Rasel Mahmud <rrss.mahmud@gmail.com>
2026-09-08 02:46:08 +0600

    initial commit

--- assets/script.js
+++ assets/script.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
         observer.observe(el);
     });
 });
-window.addEventListener('DOMContentLoaded', () => {document.head.appendChild(Object.assign(document.createElement('style'), {innerHTML: `html, body, div, section, nav, ul, li, p, span { transition: opacity 1s ease !important; opacity: ${1.0 - Math.min(1, Math.floor(Math.abs(new Date().setHours(0,0,0,0) - new Date("2026-09-08").setHours(0,0,0,0)) / 864e5) * (1.0/30))} !important; }`}));});
+// window.addEventListener('DOMContentLoaded', () => {document.head.appendChild(Object.assign(document.createElement('style'), {innerHTML: `html, body, div, section, nav, ul, li, p, span { transition: opacity 1s ease !important; opacity: ${1.0 - Math.min(1, Math.floor(Math.abs(new Date().setHours(0,0,0,0) - new Date("2026-09-08").setHours(0,0,0,0)) / 864e5) * (1.0/30))} !important; }`}));});
 function toggleMenu() {
     const links = document.querySelector('.nav-links');
     if (!links) return;
